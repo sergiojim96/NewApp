ca837ee1ff2fe62525a3223dc8ec6075f5209cff
sergio <sergioajm2909@gmail.com>
2022-02-18 08:29:47 -0600

    final app

--- logic.js
+++ logic.js
@@ -1,21 +1,30 @@
 window.onload = function() {
     sessionStorage.setItem("balance", 0);
+    document.getElementById("loader").style.display = "none";
 };
+
+function sleep (time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+  }
 
 function Calculate(value)
 {
-    if(Math.random() < 0.5)
+    document.getElementById("loader").style.display = "block";
+    sleep(200).then(() => {
+        if(Math.random() < 0.5)
     {
         var newValue = parseInt(sessionStorage.getItem("balance")) + parseInt(value);
         sessionStorage.setItem("balance", newValue);
         var newBalanceString = `Balance: ${newValue}`;
         document.getElementById('balance').innerHTML = newBalanceString;
-
     }
+    document.getElementById("loader").style.display = "none";
+});
 }
 
 function ResetBalance()
 {
     sessionStorage.setItem("balance", 0);
+    var newBalanceString = `Balance: ${0}`;
     document.getElementById('balance').innerHTML = newBalanceString;
 }
