9046c99795510aec42381680f8fec1f2e4db1072
sergio <sergioajm2909@gmail.com>
2022-02-17 23:49:11 -0600

    alaj

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const app = express();
 const path = require('path');
 const router = express.Router();
 
-router.get('*',function(req,res){
+router.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));
   //__dirname : It will resolve to your project folder.
 });
 
 //add the router
-app.use('*', router);
-app.use('*', express.static(__dirname + '*'));
-app.listen(process.env.port || 3000);
+app.use('/', router);
+app.use('/', express.static(__dirname + '/'));
+app.listen(process.env.port || 5000);
